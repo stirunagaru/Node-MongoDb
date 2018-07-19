89b5d060e2c03a5e563a8327211edb7925da567c
Sumanjali Tirunagaru <tsumanjali94@gmail.com>
2018-07-19 18:51:44 -0500

    added mlab-2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-//'sumanjali:suma@3005@ds141671.mlab.com:41671/todos-app-db';
+//'mongodb://sumanjali:suma@3005@ds141671.mlab.com:41671/todos-app-db';
 
 //||'mongodb://localhost:27017/TodoApp'
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect('mongodb://sumanjali:suma@3005@ds141671.mlab.com:41671/todos-app-db' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
