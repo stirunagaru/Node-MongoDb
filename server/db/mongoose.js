38491ce59728200f6c3941e06dabf4abd39b3456
Sumanjali Tirunagaru <tsumanjali94@gmail.com>
2018-07-19 19:38:35 -0500

    added mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,11 +5,12 @@ mongoose.Promise = global.Promise;
 //'mongodb://sumanjali:suma@3005@ds141671.mlab.com:41671/todos-app-db';
 
 //||'mongodb://localhost:27017/TodoApp'
-mongoose.connect(process.env.MONGODB_URI, (err)=>{
-  if(err){
-    //  console.log('CONNECTION ERROR:', err);
-    }
-    console.log('CONNECTED to DB!');
-});
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
+//    (err)=>{
+//   if(err){
+//     //  console.log('CONNECTION ERROR:', err);
+//     }
+//     console.log('CONNECTED to DB!');
+// });
 
 module.exports = {mongoose};
