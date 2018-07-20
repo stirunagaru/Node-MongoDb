58e8029496b0c837fedfb16a2d2b99d85eeeb168
Sumanjali Tirunagaru <tsumanjali94@gmail.com>
2018-07-19 20:01:32 -0500

    modifications

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-//'mongodb://sumanjali:suma@3005@ds141671.mlab.com:41671/todos-app-db';
+//'mongodb://tirunagaru:suma3005@ds141671.mlab.com:41671/todos-app-db';
 
 //||'mongodb://localhost:27017/TodoApp'
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect('mongodb://tirunagaru:suma3005@ds141671.mlab.com:41671/todos-app-db');
 //    (err)=>{
 //   if(err){
 //     //  console.log('CONNECTION ERROR:', err);
